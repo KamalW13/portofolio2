4b34e6da55c50c2b53e95145794467de9b0f4f68
KamalW13 <singaking101@gmail.com>
2023-04-05 17:18:00 +0700

    Commit Pertama

---
+++ script.js
@@ -0,0 +1,5 @@
+const imageThumb = document.querySelectorAll('.karya .thumb');
+imageThumb.forEach(thumbnail => {
+    const imageUrl = thumbnail.getAttribute('data-thumbnail');
+    thumbnail.style.backgroundImage = `url(${imageUrl})`;
+});
